551cc305dd6ff8b6ce53c71ebec91d631e8a22c6
Stefan Mirea <steven.mirea@gmail.com>
2012-05-10 12:16:44 +0200

    added small check

--- js/admin.js
+++ js/admin.js
@@ -19,41 +19,43 @@ $(function(){
   
   var $rooms = $('.room');
   
-  $('<div />').qtip({
-    overwrite : false,
-    content: ' ', 
-    position: {
-      target    : 'event',
-      effect    : false,
-      viewport  : $(window)
-    },
-    show: {
-      target  : $rooms,
-      event   : 'click'
-    },
-    hide: {
-      target  : $rooms,
-      event   : 'unfocus'
-    },
-    events: {
-      show: function(event, api) {
-        var $target   = $(event.originalEvent.currentTarget);
-        var cls       = $target.attr('class').split(' ');
-        var $content  = $();
-        for( var i in cls ){
-          if( /^group-[0-9]+$/.test( cls[i] ) ){
-            $content = $content.add( $('#'+cls[i]).clone() );
+  if( $().qtip ){
+    $('<div />').qtip({
+      overwrite : false,
+      content: ' ', 
+      position: {
+        target    : 'event',
+        effect    : false,
+        viewport  : $(window)
+      },
+      show: {
+        target  : $rooms,
+        event   : 'click'
+      },
+      hide: {
+        target  : $rooms,
+        event   : 'unfocus'
+      },
+      events: {
+        show: function(event, api) {
+          var $target   = $(event.originalEvent.currentTarget);
+          var cls       = $target.attr('class').split(' ');
+          var $content  = $();
+          for( var i in cls ){
+            if( /^group-[0-9]+$/.test( cls[i] ) ){
+              $content = $content.add( $('#'+cls[i]).clone() );
+            }
+          }
+          
+          if( $content.length ) {
+            api.set( 'content.text', $content );
+          } else {
+            api.set( 'content-text', '-- nobody applied for this apartment :( --' );
           }
         }
-        
-        if( $content.length ) {
-          api.set( 'content.text', $content );
-        } else {
-          api.set( 'content-text', '-- nobody applied for this apartment :( --' );
-        }
       }
-    }
-  });
+    });
+  }
   
   $('.small-group-id')
     .each(function(){
