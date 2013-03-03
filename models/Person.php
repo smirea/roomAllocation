7fedb46d3a99d8245b9ede4e10a056edd1d3509d
Dominik Kundel <dominik.kundel@me.com>
2013-03-03 23:27:01 +0100

    added methods to Person model

--- models/Person.php
+++ models/Person.php
@@ -9,18 +9,26 @@
       parent::__construct($table);
     }
 
+    public username_exists ($user) {
+      return (mysql_num_rows($this->select('id', "WHERE account='$user'")) > 0);
+    }
 
     public get ($eid) {
       return $this->to_array($this->select('*', "WHERE eid='$eid'"));
     }
 
     public search ($columns, $min_year, $clause) {
-      return $this->select($columns, "WHERE (
+      return $this->to_array($this->select($columns, "WHERE (
                                         (status='undergrad' AND year>'$min_year')
                                         OR (status='foundation-year' AND year='$min_year')
                                       )
-                                      AND $clause");
-      }
+                                      AND $clause"));
+    }
+
+    public get_countries () {
+      return $this->to_array($this->select("DISTINCT country"));
+    }
+
   }
 
 ?>
