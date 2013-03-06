60e8a65ee5f797d5ee0a773f8142aee5a588d2de
smirea <steven.mirea@gmail.com>
2013-03-06 22:29:55 +0100

    fixed syntax errors

--- models/Request_Model.php
+++ models/Request_Model.php
@@ -1,7 +1,7 @@
 <?php
 
 	require_once 'config.php';
-	require_once 'models/Model.php'
+	require_once 'models/Model.php';
 
   /**
    * Describes the roommate request between two user consisting out of the eid of the two user and a request id.
@@ -12,11 +12,11 @@
 			parent::__construct($table);
 		}
 
-		public send_request ($eid_from, $eid_to) {
+		public function send_request ($eid_from, $eid_to) {
 			return $this->insert(array("eid_from" => $eid_from, "eid_to" => $eid_to));
 		}
 
-		public request_exists ($eid, $eid_to) {
+		public function request_exists ($eid, $eid_to) {
 			$requests = $this->select('id', "WHERE (eid_from='$eid' AND eid_to='$eid_to') OR (eid_from='$eid_to' AND eid_to='$eid')");
 			if($requests){
 				return (mysql_num_rows($requests) > 0);
@@ -25,20 +25,20 @@
 			}
 		}
 
-		public is_request ($eid_from, $eid_to) {
-			$request = $this->select('id', "WHERE eid_from='$eid_from' AND eid_to='$eid_to'")
-			if($request) {
+		public function is_request ($eid_from, $eid_to) {
+			$request = $this->select('id', "WHERE eid_from='$eid_from' AND eid_to='$eid_to'");
+			if ($request) {
 				return (mysql_num_rows($request) > 0);
 			} else {
 				return Model::SQL_FAILED;
 			}
 		}
 
-		public accept_request ($eid_from, $eid_to) {
+		public function accept_request ($eid_from, $eid_to) {
 			return $this->delete("WHERE (eid_from='$eid_from' AND eid_to='$eid_to') OR (eid_from='$eid_to' AND eid_to='$eid_from')");
 		}
 
-		public remove_remaining ($eid) {
+		public function remove_remaining ($eid) {
 			return $this->delete("WHERE eid_to='$eid'");
 		}
 
