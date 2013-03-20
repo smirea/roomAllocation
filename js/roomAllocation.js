55f002ad542ad79fe6688f9cdd912865ee76947a
smirea <steven.mirea@gmail.com>
2013-03-20 22:49:10 +0100

    some style changes

--- js/roomAllocation.js
+++ js/roomAllocation.js
@@ -41,8 +41,6 @@ var RPC = {
   var $addRoommate;
   var $freshman;
   var $loading;
-  var messages = $();
-  var message_timeout;
   
   $(function(){
     alter_jquery_ui();
@@ -64,6 +62,10 @@ var RPC = {
     exports.tutorial_running = false;
 
     var tourdata = [
+      {
+        html: "Welcome to the College Choice phase. <br /> This is a step by step tutorial. <br />You can use the commands bellow every message to <b>stop</b>/<b>pause</b>/<b>rewind</b> and <b>fast forward</b> the tutorial.<br /> Enjoy!",
+        overlayOpacity: 0.8
+       },
        {
         html: "Choose any college you want to move.",
         element: $('.college-choice:nth(1)'),
@@ -94,23 +96,61 @@ var RPC = {
         position: 'e',
        },
        {
-        html: "After each swap a confirmation message for storing will be shown on top",
+        html: 'If you are not going to be on campus for a semester or going on exchange, make sure to tick the box',
+        element: $('#exchange'),
+        overlayOpacity: 0.8,
+        position: 'e',
+        expose: true
+       },
+       {
+        html: 'Also if you are considering choosing a room on a quiet floor, tick here. This is by no means binding, it is just so the College Masters have an idea on the overall preferences',
+        element: $('#quiet_zone'),
+        overlayOpacity: 0.8,
+        position: 'e',
+        expose: true
+       },
+       {
+        html: "After each action your preferences are automatically shown and a confirmation message for storing will be shown on top.",
+        overlayOpacity: 0.8,
         onBeforeShow: function(element) {
           message("info", "College prefences updated!")
+        },
+       },
+       {
+          html: 'When in doubt, you can always re-start the tour from here',
+          element: $('#beginTour'),
+          overlayOpacity: 0.8,
+          position: 'w',
+          expose: true
         }
-       }
     ];
 
-
-    var tour = jTour(tourdata ,{
-      axis:'y',  // use only one axis prevent flickring on iOS devices
-      onStart: function (current) {
-        $("#college_choices_sort").sortable( "disable" );
-      },
-      onStop: function (current) {
-        $("#college_choices_sort").sortable( "enable" );
+    var key_name = window.location.origin + window.location.pathname;
+    var tutorial_opts = {
+      axis: 'y',  // use only one axis prevent flickring on iOS devices
+      autostart: true
+    };
+    if (window.localStorage) {
+      if (window.localStorage[key_name]) {
+        var storage = JSON.parse(window.localStorage[key_name]);
+        if (storage.tutorial) {
+          tutorial_opts.autostart = false;
+        }
       }
-    });
+      window.localStorage[key_name] = JSON.stringify({tutorial:true});
+    }
+
+    var tour = jTour(
+      tourdata,
+      Object.create(tutorial_opts, {
+        onStart: function (current) {
+          $("#college_choices_sort").sortable( "disable" );
+        },
+        onStop: function (current) {
+          $("#college_choices_sort").sortable( "enable" );
+        }
+      })
+    );
 
     exports.tutorial = tour;
 
@@ -160,6 +200,16 @@ var RPC = {
   }
 
   var bulk_events = function(){
+    var checkbox_selector = '.college-additional-options input[type="checkbox"]';
+    $(document).on('change', checkbox_selector, function () {
+      if ($(this).attr('checked') === 'checked') {
+        $(this).parent().addClass('checked');
+      } else {
+        $(this).parent().removeClass('checked');
+      }
+    });
+    $(checkbox_selector+':checked').parent().addClass('checked');
+
     $('.message .close').live('click.close', function(){
       var $msg = $(this).parent();
       $msg.fadeOut( 600, function(){ $msg.remove(); } );
@@ -484,24 +534,26 @@ var RPC = {
     }
   })();
   
-  function message( type, message ){
-    var msg = messages.filter('.'+type);
-    if( msg.length > 0 ){
-      var container = msg.parent();
-      var clone     = msg.clone();
-      clone
-        .appendTo( container )
-        .hide()
-        .fadeIn( 800 )
-        .find('.content')
-        .html( message );
-      setTimeout(function(){
-        clone.fadeOut();
-      }, 5000 + msg.length * 10);
-      container[0].scrollTop = container[0].scrollHeight;
-    } else {
-      console.warn( 'Unknown message type', arguments );
-    }
-  }
-  
 })( jQuery );
+
+var messages = $();
+var message_timeout;
+function message( type, message ){
+  var msg = messages.filter('.'+type);
+  if( msg.length > 0 ){
+    var container = msg.parent();
+    var clone     = msg.clone();
+    clone
+      .appendTo( container )
+      .hide()
+      .fadeIn( 800 )
+      .find('.content')
+      .html( message );
+    setTimeout(function(){
+      clone.slideUp();
+    }, 5000 + msg.length * 10);
+    container[0].scrollTop = container[0].scrollHeight;
+  } else {
+    console.warn( 'Unknown message type', arguments );
+  }
+}
