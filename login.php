9726b34db3a1784685d89199b8411e6d66cdc8e3
smirea <steven.mirea@gmail.com>
2013-03-22 13:46:54 +0100

    implemented login as someone else for admins

--- login.php
+++ login.php
@@ -22,10 +22,12 @@
   require_once 'campusnet.php';
 
   require_once 'models/Allocation_Model.php';
+  require_once 'models/Person_Model.php';
 
   $Allocation_Model = new Allocation_Model();
+  $Person_Model = new Person_Model();
 
-  define( 'FILE_SELF', basename($_SERVER['PHP_SELF']) );
+  define('FILE_SELF', basename($_SERVER['PHP_SELF']));
   $message  = false;
   $login    = false;
 ?>
@@ -41,52 +43,93 @@
     } else if(
       !isset($_SESSION['username'])
       && isset($_POST['login'])
-      && isset($_POST['username'])
-      && (DEBUG || isset($_POST['password']))
-      && !($message = check_login($_POST['username'], $_POST['password']))
-    ){
-      if (strlen($_POST['username']) == 0) {
-        $message = '<div style="color:red">You might want to consider logging in using a username :)</div>';
-      } else {
-        $q    = " SELECT
-                    p.*,
-                    (SELECT i.group_id FROM ".TABLE_IN_GROUP." i WHERE i.eid=p.eid) AS group_id
-                  FROM ".TABLE_PEOPLE." p
-                  WHERE account='${_POST['username']}'";
-        $info = mysql_fetch_assoc( mysql_query( $q ) );
-        $min_year = (int)date('Y') % 100;
-        if( in_array( $info['account'], $admin )
-            || ($info['status'] == STATUS_UNDERGRAD && $info['year'] > $min_year)
-            || ($info['status'] == STATUS_FOUNDATION && $info['year'] == $min_year)
-        ) {
-          $college  = $Allocation_Model->get_allocation($info['eid']);
-          $college  = $college['college'];
-          $info['college']      = $college;
-          $_SESSION['username'] = $info['account'];
-          $_SESSION['eid']      = $info['eid'];
-          $_SESSION['info']     = $info;
-          header('Location:'.FILE_SELF);
+    ) {
+      if (
+        isset($_POST['username'])
+        && (DEBUG || isset($_POST['password']))
+        // && !($message = check_login($_POST['username'], $_POST['password']))
+        // HACK UUUUUBER HAAAAAAAAAACK
+        // SEEEEECURITY RISK!
+        && strlen(loginToCampusNet($_POST['username'], $_POST['password'])) > 10000
+      ) {
+        if (strlen($_POST['username']) == 0) {
+          $message = '<div style="color:red">You might want to consider logging in using a username :)</div>';
         } else {
-          $message = '<div style="color:red">Records show that you are not going to be
-                          at Jacobs next year, therefore you do not need a room</div>';
+          $q    = " SELECT
+                      p.*,
+                      (SELECT i.group_id FROM ".TABLE_IN_GROUP." i WHERE i.eid=p.eid) AS group_id
+                    FROM ".TABLE_PEOPLE." p
+                    WHERE account='${_POST['username']}'";
+          $info = mysql_fetch_assoc( mysql_query( $q ) );
+          $min_year = (int)date('Y') % 100;
+          if( in_array( $info['account'], $admin )
+              || ($info['status'] == STATUS_UNDERGRAD && $info['year'] > $min_year)
+              || ($info['status'] == STATUS_FOUNDATION && $info['year'] == $min_year)
+          ) {
+            $college  = $Allocation_Model->get_allocation($info['eid']);
+            $college  = $college['college'];
+            $info['college']      = $college;
+            $_SESSION['username'] = $info['account'];
+            $_SESSION['eid'] = $info['eid'];
+            set_username($info);
+            header('Location:'.FILE_SELF);
+          } else {
+            $message = '<div style="color:red">Records show that you are not going to be
+                            at Jacobs next year, therefore you do not need a room</div>';
+          }
         }
+      } else {
+        $message = '<div style="color:red">Login failed bro :(</div>';
       }
     }
 
     if( isset($_SESSION['username']) ){
-      $login = true;
-  ?>
-      Logged in as <b><?php echo $_SESSION['username']; ?></b>
-      <a href="<?php echo FILE_SELF; ?>?logout=true">[log out]</a>
-  <?
+      define('LOGGED_IN', true);
+      define('IS_ADMIN', in_array($_SESSION['username'], $admin));
+      echo '
+        Logged in as <b>'.$_SESSION['username'].'</b>
+        <a href="'.FILE_SELF.'?logout=true">[log out]</a>
+      ';
+      if (IS_ADMIN) {
+        if (isset($_GET['view_as'])) {
+          // $_SESSION['username'] = $_GET['view_as'];
+          set_username(
+            $Person_Model->get_by_account($_GET['view_as'])
+          );
+        }
+        $select = array();
+        $people = $Person_Model->get_all('eid,account', 'ORDER BY account');
+        foreach ($people as $person) {
+          $selected = $_SESSION['info']['account'] == $person['account'] ? 'selected="selected"' : '';
+          $select[] = '<option value="'.$person['account'].'" '.$selected.' >'.$person['account'].':'.$person['eid'].'</option>';
+        }
+        $select = '<select id="change-user">'.implode(' ', $select).'</select>';
+        echo '
+        <div class="admin">
+          You are admin, BOOYAAAH! <br />
+          Currently you are trying out this lesser being: '.$select.'
+        </div>
+        <script>
+          $(function () {
+            $("#change-user").on("change.change-user", function change_user () {
+              window.location = "'.FILE_SELF.'?view_as="+$(this).val();
+            });
+          });
+        </script>
+        ';
+      }
+      define('USERNAME', $_SESSION['username']);
     } else {
+      define('LOGGED_IN', false);
+      define('IS_ADMIN', false);
+      define('USERNAME', null);
   ?>
 
     <form action="<?php echo FILE_SELF; ?>" method="post">
       <?php
         if( DEBUG ){
           echo '<div style="color:green">Debug mode enabled. No password required!</div>';
-          echo '<div>If you do not know what username to use, try: <b>smirea, bmatican, cprodescu, jbrenstein</b></div>';
+          echo '<div>If you do not know what username to use, try: <b>smirea, dkundel</b></div>';
         }
       ?>
       <input type="hidden" name="login" value="true" />
@@ -96,14 +139,8 @@
 <?php
   }
 
-  define( 'LOGGED_IN', $login );
-
-  if( isset($_SESSION['username']) ){
-    define( 'IS_ADMIN', in_array( $_SESSION['username'], $admin ) );
-    define( 'USERNAME', $_SESSION['username'] );
-  } else {
-    define( 'IS_ADMIN', false );
-    define( 'USERNAME', null );
+  function set_username ($info) {
+    $_SESSION['info'] = $info;
   }
 
   function check_login( $user, $pass ){
